69f02c7a3145e8330bcf15b6052f63d2a8426e5a
Waqas Ahmed <wahmed@sequelmed.com>
2017-10-10 23:06:25 +0500

    fixes #1
    branch information, suppose to work in detach mode also

--- lib/git-revision.rb
+++ lib/git-revision.rb
@@ -29,8 +29,13 @@ module Git
         `git describe --tags --abbrev=0`.strip
       end
 
+      def branch
+        `git ls-remote --heads origin | grep $(git rev-parse HEAD) | cut -d / -f 3`
+      end
+
       def info
         @info ||= {
+          :branch             => branch,
           :commit_hash        => commit,
           :commit_hash_short  => commit_short,
           :commit_subject     => message,
